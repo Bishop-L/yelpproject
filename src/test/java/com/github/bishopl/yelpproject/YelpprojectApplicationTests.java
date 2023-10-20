b5eb551dc743d64aefa9877228693a1143dd30a0
Bishop <bishop.lawson@amcsgroup.com>
2023-10-19 17:42:40 -0700

    First Commit, Mostly framework at this point.

---
+++ src/test/java/com/github/bishopl/yelpproject/YelpprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bishopl.yelpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelpprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
